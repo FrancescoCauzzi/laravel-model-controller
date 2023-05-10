ad0ebe3f8e55acf3932aabccab3a783cd1df286a
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-05-10 17:02:32 +0200

    feat: cards visualizzate

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,13 +6,24 @@
     <h1>
         Movies DB
     </h1>
-    <ul>
+
+    <div class="d-flex flex-wrap justify.content-center __movie-list">
         @foreach ($movies as $movie)
-        <li>
-            {{$movie}}
-        </li>
+        <div>
+            <div class="card __card">
+                {{-- <img src="..." class="card-img-top" alt="..."> --}}
+                <div class="card-body">
+                  <h5 class="card-title">{{$movie->title}}</h5>
+                  <h6>Original title: {{$movie->original_title}}</h6>
+                  <p>Nationality: {{$movie->nationality}}</p>
+                  <p>Year: {{$movie->date}}</p>
+                  <p>Rate: {{$movie->vote}}</p>
+
+                </div>
+              </div>
+        </div>
         @endforeach
-    </ul>
+    </div>
 </div>
 
 
